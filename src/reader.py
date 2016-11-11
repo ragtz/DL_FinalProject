14a84d186f1f3d98b042405bb70d45a3d97391e0
ragtz <agrobomaniac@gmail.com>
2016-11-11 13:45:39 -0600

    Restructure batching

--- src/reader.py
+++ src/reader.py
@@ -8,14 +8,25 @@ def img_producer(raw_data, batch_size, num_steps, shuffle=False, name="IMGProduc
 
         samples_per_image = (cols - 1) / num_steps
         epoch_size = N*samples_per_image/batch_size
-        print epoch_size
 
         assertion = tf.assert_positive(epoch_size)
         with tf.control_dependencies([assertion]):
             epoch_size = tf.identity(epoch_size, name="epoch_size")
 
-        X = tf.reshape(raw_data[:,:,0:num_steps*samples_per_image], [epoch_size, batch_size, rows, num_steps])
-        Y = tf.reshape(raw_data[:,:,1:num_steps*samples_per_image+1], [epoch_size, batch_size, rows, num_steps])        
+        # get all samples and flatten to 2-D
+        X = tf.reshape(raw_data[:,:,0:num_steps*samples_per_image], [rows, N*num_steps*samples_per_image])
+        Y = tf.reshape(raw_data[:,:,1:num_steps*samples_per_image+1], [rows, N*num_steps*samples_per_image])
+
+        # remove excess data
+        X = tf.slice(X, [0,0], [rows, epoch_size*batch_size*num_steps])
+        Y = tf.slice(Y, [0,0], [rows, epoch_size*batch_size*num_steps])
+
+        # reshape to batches
+        X = tf.reshape(X, [epoch_size, batch_size, rows, num_steps])
+        Y = tf.reshape(Y, [epoch_size, batch_size, rows, num_steps])
+
+        #X = tf.reshape(raw_data[:,:,0:num_steps*samples_per_image], [epoch_size, batch_size, rows, num_steps])
+        #Y = tf.reshape(raw_data[:,:,1:num_steps*samples_per_image+1], [epoch_size, batch_size, rows, num_steps])        
 
         i = tf.train.range_input_producer(epoch_size, shuffle=shuffle).dequeue()
         x = tf.slice(X, [i,0,0,0], [1, batch_size, rows, num_steps])
