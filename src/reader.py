f0abf2cac6595cb5a4d95b9ce47f47193dc53d80
ragtz <agrobomaniac@gmail.com>
2016-11-11 00:06:11 -0600

    Fix typo

--- src/reader.py
+++ src/reader.py
@@ -7,7 +7,7 @@ def img_producer(raw_data, batch_size, num_steps, shuffle=False, name="IMGProduc
         raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
 
         samples_per_image = (cols - 1) / num_steps
-        epoch_size = N*samples_per_image/bacth_size
+        epoch_size = N*samples_per_image/batch_size
 
         assertion = tf.assert_positive(epoch_size, message="epoch_size == 0, decrease batch_size or num_steps")
         with tf.control_dependencies([assertion]):
