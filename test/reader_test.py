3b8c7790219a784e721836e774d5aaa6c58020ba
ragtz <agrobomaniac@gmail.com>
2016-11-11 15:57:43 -0600

    Add tests

--- test/reader_test.py
+++ test/reader_test.py
@@ -17,6 +17,8 @@ class ImgReaderTest(tf.test.TestCase):
                                    [15,16,17,18,19]]])
 
     def testImgProducer0(self):
+        print "Test 0"
+
         batch_size = 3
         num_steps = 2
 
@@ -67,6 +69,8 @@ class ImgReaderTest(tf.test.TestCase):
                 coord.join(threads)
 
     def testImgProducer1(self):
+        print "Test 1"
+
         batch_size = 2
         num_steps = 2
 
@@ -129,6 +133,8 @@ class ImgReaderTest(tf.test.TestCase):
                 coord.join(threads)
 
     def testImgProducer2(self):
+        print "Test 2"
+
         batch_size = 2
         num_steps = 3
 
@@ -165,6 +171,86 @@ class ImgReaderTest(tf.test.TestCase):
             finally:
                 coord.request_stop()
                 coord.join(threads)
+
+    def testImgProducer3(self):
+        print "Test 3"
+
+        batch_size = 5
+        num_steps = 2
+
+        x, y = reader.img_producer(self.raw_data, batch_size, num_steps)
+
+        with self.test_session() as session:
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(session, coord=coord)
+            
+            try:
+                xval, yval = session.run([x, y])
+                self.assertAllEqual(xval, [[[0,1],
+                                            [0,1]],
+ 
+                                           [[2,3],
+                                            [2,3]],
+
+                                           [[5,6],
+                                            [5,6]]
+
+                                           [[7,8],
+                                            [7,8]],
+
+                                           [[10,11],
+                                            [10,11]]])
+                self.assertAllEqual(yval, [[[1,2],
+                                            [1,2]],
+ 
+                                           [[3,4],
+                                            [3,4]],
+
+                                           [[6,7],
+                                            [6,7]]
+
+                                           [[8,9],
+                                            [8,9]],
+
+                                           [[11,12],
+                                            [11,12]]])
+            finally:
+                coord.request_stop()
+                coord.join(threads)
+
+    def testImgProducer4(self):
+        print "Test 4"
+
+        batch_size = 3
+        num_steps = 4
+
+        x, y = reader.img_producer(self.raw_data, batch_size, num_steps)
+
+        with self.test_session() as session:
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(session, coord=coord)
+            
+            try:
+                xval, yval = session.run([x, y])
+                self.assertAllEqual(xval, [[[0,1,2,3],
+                                            [0,1,2,3]],
+ 
+                                           [[5,6,7,8],
+                                            [5,6,7,8]],
+
+                                           [[10,11,12,13],
+                                            [10,11,12,13]]])
+                self.assertAllEqual(yval, [[[1,2,3,4],
+                                            [1,2,3,4]],
+ 
+                                           [[6,7,8,9],
+                                            [6,7,8,9]],
+
+                                           [[11,12,13,14],
+                                            [11,12,13,14]]])
+            finally:
+                coord.request_stop()
+                coord.join(threads)    
 
 if __name__ == '__main__':
     tf.test.main()
