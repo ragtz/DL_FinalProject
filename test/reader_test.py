e984af7c6102dfb8e9b9ee0aacc498dc19e13c6d
ragtz <agrobomaniac@gmail.com>
2016-11-11 13:56:17 -0600

    Change what is returned and printed

--- test/reader_test.py
+++ test/reader_test.py
@@ -26,6 +26,10 @@ class ImgReaderTest(tf.test.TestCase):
             
             try:
                 xval, yval = session.run([x, y])
+                print xval
+                print yval
+                print
+                '''
                 self.assertAllEqual(xval, [[[0,1],
                                             [0,1]],
  
@@ -42,8 +46,12 @@ class ImgReaderTest(tf.test.TestCase):
 
                                            [[6,7],
                                             [6,7]]])
-
+                '''
                 xval, yval = session.run([x, y])
+                print xval
+                print yval
+                print
+                '''
                 self.assertAllEqual(xval, [[[7,8],
                                             [7,8]],
  
@@ -60,6 +68,7 @@ class ImgReaderTest(tf.test.TestCase):
 
                                            [[13,14],
                                             [13,14]]])
+                '''
             finally:
                 coord.request_stop()
                 coord.join(threads)
