7a2c973a1fa56ebd61187450b53628ce9e70cc99
ragtz <agrobomaniac@gmail.com>
2016-11-11 00:56:27 -0600

    Add [self] to join

--- test/reader_test.py
+++ test/reader_test.py
@@ -66,7 +66,7 @@ class ImgReaderTest(tf.test.TestCase):
                                             [13,14]]])
             finally:
                 coord.request_stop()
-                #coord.join()
+                coord.join([self])
 
 if __name__ == '__main__':
     tf.test.main()
