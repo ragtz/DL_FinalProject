b89f509d6110a404ad30939828e77ef1e85a9c64
ragtz <agrobomaniac@gmail.com>
2016-11-11 19:16:51 -0600

    Fix typo

--- src/reader.py
+++ src/reader.py
@@ -40,8 +40,8 @@ def img_producer(raw_data, batch_size, num_steps, shuffle=False, name="IMGProduc
         Y = tf.transpose(Y, perm=[0,1,3,2])      
 
         i = tf.train.range_input_producer(epoch_size, shuffle=shuffle).dequeue()
-        x = tf.slice(X, [i,0,0,0], [batch_size, rows, num_steps]))
-        y = tf.slice(Y, [i,0,0,0], [batch_size, rows, num_steps]))
+        x = tf.slice(X, [i,0,0,0], [batch_size, rows, num_steps])
+        y = tf.slice(Y, [i,0,0,0], [batch_size, rows, num_steps])
 
         return x, y
 
