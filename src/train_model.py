48632b96d0e7f1b42394c62082c8b6a8f855797e
ragtz <agrobomaniac@gmail.com>
2016-11-12 16:33:19 -0600

    Add model saving

--- src/train_model.py
+++ src/train_model.py
@@ -2,22 +2,27 @@ from DL_FinalProject.src.lstmModel import *
 import DL_FinalProject.config.config as config
 import tensorflow as tf
 
-tf.app.flags.DEFINE_string('model', 'test', 'Model config')
+tf.app.flags.DEFINE_string('config', 'test', 'Model config')
 tf.app.flags.DEFINE_string('data', None, 'Data numpy file')
+tf.app.flags.DEFINE_string('name', None, 'Name of saved model')
 FLAGS = tf.app.flags.FLAGS
 
 def main(argv):
     session = tf.Session()
 
     print "Load input"
-    lstm_input = LSTMInput(config.configs[FLAGS.model], FLAGS.data)
+    lstm_input = LSTMInput(config.configs[FLAGS.config], FLAGS.data)
 
     print "Init Model"
-    lstm_model = LSTMModel(config.configs[FLAGS.model], lstm_input, session)
+    lstm_model = LSTMModel(config.configs[FLAGS.config], lstm_input, session)
     session.run(tf.initialize_all_variables())
+
+    saver = tf.train.Saver(tf.all_variables())
 
     #print "Train epoch:", lstm_input.epoch_size, "batches of size", lstm_input.feature_vector_size, "x", lstm_input.num_steps
     lstm_model.train()
+
+    saver.save(sess, FLAGS.name + '.ckpt')
 
     session.close()
 
