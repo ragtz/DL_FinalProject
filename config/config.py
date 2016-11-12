7c7c2a9c9bee390c6fe1c0bf1c8aaad0b95d4279
ragtz <agrobomaniac@gmail.com>
2016-11-11 20:20:42 -0600

    Add config files

---
+++ config/config.py
@@ -0,0 +1,4 @@
+from DL_FinalProject.config import test_config
+
+configs = {'test': test_config.TestLSTMConfig}
+
---
+++ config/test_config.py
@@ -0,0 +1,8 @@
+
+class TestLSTMConfig(object):
+    learning_rate = 1.0
+    hidden_size = 256
+    num_layers = 2
+    batch_size = 20
+    num_steps = 10
+
