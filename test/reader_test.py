838feb17c9b2eb7ca39459602b0e1f5dc2cc0a01
ragtz <agrobomaniac@gmail.com>
2016-11-11 22:10:54 -0600

    Change test to full dataste

--- test/reader_test.py
+++ test/reader_test.py
@@ -263,8 +263,8 @@ class ImgReaderTest(tf.test.TestCase):
                 coord.join(threads)
 
     def testRealImg(self):
-        data = np.load('../data/test/test.npy')
-        batch_size = 15
+        data = np.load('../data/spectrograms/spectrograms.npy')
+        batch_size = 256
         num_steps = 32
         epoch_size = reader.epoch_size(data, batch_size, num_steps)
 
