09b97e53642fc4d99d2d19ce8e29d9a9af295f6e
ragtz <agrobomaniac@gmail.com>
2016-11-11 21:36:04 -0600

    Change optimizer

--- src/lstmModel.py
+++ src/lstmModel.py
@@ -37,7 +37,7 @@ class LSTMModel(object):
             targets_reshaped = tf.reshape(self.lstm_input.targets, [-1, self.lstm_input.feature_vector_size])
 
             self.loss = tf.reduce_mean(tf.nn.l2_loss(tf.sub(network_output, targets_reshaped)))
-            self.train_op = tf.train.GradientDescentOptimizer(self.config.learning_rate).minimize(self.loss)
+            self.train_op = tf.train.RMSPropOptimizer(self.config.learning_rate).minimize(self.loss)
 
     def train_epoch(self):
         print "--------------------"
