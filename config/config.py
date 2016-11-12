07b9026edf2c0daa473969c7b8a54c753e483637
ragtz <agrobomaniac@gmail.com>
2016-11-12 15:51:54 -0600

    Add new model config

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 from DL_FinalProject.config import test_config
+from DL_FinalProject.config import model1
 
-configs = {'test': test_config.TestLSTMConfig}
+configs = {'test': test_config.TestLSTMConfig, 'model1': model1.ModelOneLSTMConfig}
 
---
+++ config/model1.py
@@ -0,0 +1,11 @@
+
+class ModelOneLSTMConfig(object):
+    learning_rate = 0.01
+    decay = 0.9
+    momentum = 0.0
+    hidden_size = 256
+    num_layers = 3
+    batch_size = 128
+    num_steps = 50
+    max_epoch = 100
+
