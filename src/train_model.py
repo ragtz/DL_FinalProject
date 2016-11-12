8315b8c4aa81ffde81d7c9b189d51debf58c79d2
ragtz <agrobomaniac@gmail.com>
2016-11-11 21:15:19 -0600

    Add print statements

--- src/train_model.py
+++ src/train_model.py
@@ -9,9 +9,13 @@ FLAGS = tf.app.flags.FLAGS
 def main(argv):
     session = tf.Session()
 
+    print "Load input"
     lstm_input = LSTMInput(config.configs[FLAGS.model], FLAGS.data)
+
+    print "Init Model"
     lstm_model = LSTMModel(config.configs[FLAGS.model], lstm_input, session)
 
+    print "Train epoch:", lstm_input.epoch_size
     lstm_model.train_epoch()
 
     session.close()
