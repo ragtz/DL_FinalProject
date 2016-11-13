920d55aeca8009731ecb104b8d21cbdfaa7caee0
ragtz <agrobomaniac@gmail.com>
2016-11-12 21:06:17 -0600

    Change models

--- config/model4.py
+++ config/model4.py
@@ -3,7 +3,7 @@ class ModelFourLSTMConfig(object):
     learning_rate = 0.001
     decay = 0.9
     momentum = 0
-    hidden_size = 256
+    hidden_size = 512
     num_layers = 3
     batch_size = 64
     num_steps = 200
