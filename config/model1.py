cee255fde28241f4e9680704943613f0f145856f
ragtz <agrobomaniac@gmail.com>
2016-11-12 19:46:34 -0600

    Change hidden_size

--- config/model1.py
+++ config/model1.py
@@ -3,9 +3,9 @@ class ModelOneLSTMConfig(object):
     learning_rate = 0.001
     decay = 0.9
     momentum = 0
-    hidden_size = 256
+    hidden_size = 1024
     num_layers = 3
     batch_size = 128
     num_steps = 50
-    max_epoch = 100
+    max_epoch = 200
 
