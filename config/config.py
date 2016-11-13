d0994b1eed0ab7281eeb27a46b04354e9cf22102
ragtz <agrobomaniac@gmail.com>
2016-11-13 17:54:12 -0600

    Add new model config

--- config/config.py
+++ config/config.py
@@ -3,10 +3,12 @@ from DL_FinalProject.config import model1
 from DL_FinalProject.config import model2
 from DL_FinalProject.config import model3
 from DL_FinalProject.config import model4
+from DL_FinalProject.config import model9
 
 configs = {'test': test_config.TestLSTMConfig, 
            'model1': model1.ModelOneLSTMConfig,
            'model2': model2.ModelTwoLSTMConfig,
            'model3': model3.ModelThreeLSTMConfig,
-           'model4': model4.ModelFourLSTMConfig}
+           'model4': model4.ModelFourLSTMConfig,
+           'model9': model9.ModelNineLSTMConfig}
 
---
+++ config/model9.py
@@ -0,0 +1,11 @@
+
+class ModelNineLSTMConfig(object):
+    learning_rate = 0.001
+    decay = 0.9
+    momentum = 0
+    hidden_size = 512
+    num_layers = 3
+    batch_size = 64
+    num_steps = 495
+    max_epoch = 5000
+
