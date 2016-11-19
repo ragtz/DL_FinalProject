56c70c8a5826171436cd89f9978fad61e4b493cc
ragtz <agrobomaniac@gmail.com>
2016-11-18 20:11:53 -0600

    Change to get_variable in discriminator

--- src/lstmGanModel.py
+++ src/lstmGanModel.py
@@ -59,20 +59,26 @@ class LSTMGANModel(object):
         xbatch_reshaped = tf.reshape(xbatch, [-1, self.config.width, self.lstmgan_input.feature_vector_size, 1])
 
         conv1_shape = [5,5,1,16]
-        conv1_W = tf.Variable(tf.random_normal(conv1_shape, stddev=0.01))
-        conv1_B = tf.Variable(tf.random_normal((conv1_shape[-1],), stddev=0.01))
+        #conv1_W = tf.Variable(tf.random_normal(conv1_shape, stddev=0.01))
+        #conv1_B = tf.Variable(tf.random_normal((conv1_shape[-1],), stddev=0.01))
+        conv1_W = tf.get_variable("conv1_W", conv1_shape, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+        conv1_B = tf.get_variable("conv1_B", (conv1_shape[-1],), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
         conv1 = tf.nn.relu(tf.nn.conv2d(xbatch_reshaped, conv1_W, strides=[1,1,1,1], padding='SAME') + conv1_B)
         conv1_P = tf.nn.max_pool(conv1, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
         conv2_shape = [5,5,16,32]
-        conv2_W = tf.Variable(tf.random_normal(conv2_shape, stddev=0.01))
-        conv2_B = tf.Variable(tf.random_normal((conv2_shape[-1],), stddev=0.01))
+        #conv2_W = tf.Variable(tf.random_normal(conv2_shape, stddev=0.01))
+        #conv2_B = tf.Variable(tf.random_normal((conv2_shape[-1],), stddev=0.01))
+        conv2_W = tf.get_variable("conv2_W", conv2_shape, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+        conv2_B = tf.get_variable("conv2_B", (conv2_shape[-1],), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
         conv2 = tf.nn.relu(tf.nn.conv2d(conv1_P, conv2_W, strides=[1,1,1,1], padding='SAME') + conv2_B)
         conv2_P = tf.nn.max_pool(conv2, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
         conv3_shape = [5,5,32,64]
-        conv3_W = tf.Variable(tf.random_normal(conv3_shape, stddev=0.01))
-        conv3_B = tf.Variable(tf.random_normal((conv3_shape[-1],), stddev=0.01))
+        #conv3_W = tf.Variable(tf.random_normal(conv3_shape, stddev=0.01))
+        #conv3_B = tf.Variable(tf.random_normal((conv3_shape[-1],), stddev=0.01))
+        conv3_W = tf.get_variable("conv3_W", conv3_shape, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+        conv3_B = tf.get_variable("conv3_B", (conv3_shape[-1],), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
         conv3 = tf.nn.relu(tf.nn.conv2d(conv2_P, conv3_W, strides=[1,1,1,1], padding='SAME') + conv3_B)
         conv3_P = tf.nn.max_pool(conv3, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
 
@@ -92,8 +98,10 @@ class LSTMGANModel(object):
         fc2 = tf.sigmoid(tf.matmul(tf.reshape(fc1, [-1, fc2_shape[0]]), fc2_W) + fc2_B)
         '''
         fc2_shape = [w*h*64, 1]
-        fc2_W = tf.Variable(tf.random_normal(fc2_shape, stddev=0.01))
-        fc2_B = tf.Variable(tf.random_normal((fc2_shape[-1],), stddev=0.01))
+        #fc2_W = tf.Variable(tf.random_normal(fc2_shape, stddev=0.01))
+        #fc2_B = tf.Variable(tf.random_normal((fc2_shape[-1],), stddev=0.01))
+        fc2_W = tf.get_variable("fc2_W", fc2_shape, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
+        fc2_B = tf.get_variable("fc2_B", (fc2_shape[-1],), initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01))
         fc2 = tf.sigmoid(tf.matmul(tf.reshape(conv3_P, [-1, fc2_shape[0]]), fc2_W) + fc2_B)
 
         return fc2
