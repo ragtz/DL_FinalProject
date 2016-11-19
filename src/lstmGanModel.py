07efc6d81b3819f5c96b9c5d925eacb48abdd5b1
ragtz <agrobomaniac@gmail.com>
2016-11-18 19:49:24 -0600

    Add d2 back to d_loss

--- src/lstmGanModel.py
+++ src/lstmGanModel.py
@@ -42,7 +42,7 @@ class LSTMGANModel(object):
 
             print len(tf.trainable_variables())
 
-        self.d_loss = -(tf.reduce_mean(tf.log(self.d1_outputs)))# + tf.log(1 - self.d2_outputs)))
+        self.d_loss = -(tf.reduce_mean(tf.log(self.d1_outputs) + tf.log(1 - self.d2_outputs)))
         #self.d_loss = tf.reduce_mean(tf.nn.relu(self.d1_outputs) - self.d1_outputs + tf.log(1.0 + tf.exp(-tf.abs(self.d1_outputs)))) + \
         #              tf.reduce_mean(tf.nn.relu(self.d2_outputs) + tf.log(1.0 + tf.exp(-tf.abs(self.d2_outputs))))
         #self.g_loss = tf.reduce_mean(tf.log(1 - self.d2_outputs) + tf.nn.l2_loss(tf.sub(tf.slice(self.d2_outputs), tf.slice(self.ybatch))))
