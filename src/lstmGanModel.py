98b35505f6e2c8d20eaac875846b89250424f1d5
ragtz <agrobomaniac@gmail.com>
2016-11-25 03:33:58 -0600

    Add print statements

--- src/lstmGanModel.py
+++ src/lstmGanModel.py
@@ -122,7 +122,8 @@ class LSTMGANModel(object):
             print self.config.width - 1
             print self.config.lstm_size
             print (self.config.width - 1)*self.config.lstm_size
-            print tf.shape(outputs[:,(self.config.width-1)*self.config.lstm_size:,:])
+            last_start = (self.config.width - 1)*self.config.lstm_size
+            print tf.shape(outputs[:,last_start:,:])
             outputs_reshaped = tf.reshape(outputs[:,(self.config.width-1)*self.config.lstm_size:,:], [1, self.config.lstm_size])
             network_output = tf.sigmoid(tf.matmul(outputs_reshaped, rnn_out_W) + rnn_out_B)
 
